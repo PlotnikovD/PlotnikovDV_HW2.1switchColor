48c6447f20372440675321ef89edfdcc3a7972c7
D.Plotnikov <plotnikdima5296@gmail.com>
2021-10-22 22:23:37 +0500

    Initial Commit

---
+++ PlotnikovDV_HW2.1switchColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlotnikovDV_HW2.1switchColor
+//
+//  Created by Dmitrii on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
