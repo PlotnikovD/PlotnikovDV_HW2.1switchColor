3cab8a65991f854ce8d192403a217bf1fdef445c
D.Plotnikov <plotnikdima5296@gmail.com>
2021-10-22 23:29:24 +0500

    first commit

--- PlotnikovDV_HW2.1switchColor/ViewController.swift
+++ PlotnikovDV_HW2.1switchColor/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLable: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLable: UILabel!
+    @IBOutlet var greenLable: UILabel!
+    @IBOutlet var blueLable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func redSlideAction() {
+        mainLable.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+        redLable.text = String(redSlider.value)
+        
+    }
 
+    
+    @IBAction func greenSlideAction() {
+        mainLable.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+        greenLable.text = String(greenSlider.value)
+    }
+    
+    @IBAction func blueSlideAction() {
+        mainLable.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+        blueLable.text = String(blueSlider.value)
+    }
 }
-
